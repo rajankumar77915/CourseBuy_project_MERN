b00738f0f3c3aaab9e27ea0aeba17c8dbfe471d6
rajankumar <rajankumar77915@gmail.com>
2023-09-16 19:33:15 +0530

    payment.js is updated

--- server/controllers/razorPay/Payment.js
+++ server/controllers/razorPay/Payment.js
@@ -18,9 +18,11 @@ exports.capturePayment=async(req,res)=>{
             message:"please provide valid courseId"
         })
     };
+    
     //valid courseDetail
+    let course;
     try{
-        const course=await  Course.findById({courseId});
+         course=await  Course.findById({courseId});
         if(!course){
             return res.json({
                 sucess:false,
@@ -37,6 +39,13 @@ exports.capturePayment=async(req,res)=>{
                 message:"student is already enrolled"
             })
         }
+    }catch(error){
+        return res.json({
+            sucess:true,
+            message:`error at capturePayment  when validate courseDetail :${error}`
+        })
+    }
+    
 
 
         
@@ -44,11 +53,91 @@ exports.capturePayment=async(req,res)=>{
         const amount=course.price;
         const currency="INR";
         
-    }catch(error){
+        const options={
+            amount: amount*100,
+            currency,
+            receipt:Math.random(Date.now()).toString(),
+            notes:{
+                courseId:courseId,
+                userId
+            }
+        }
 
-    }
+        try{
+            //initiate the payment using razorpay
+            const paymentResponse = await instance.orders.create(options);
+            console.log("paymentResponse",paymentResponse);
+
+            //return response
+            return res.status(200).json({
+                sucess:true,
+                courseName:course.courseName,
+                courseDescribtion:course.courseDescribtion ,
+                thumbnail:course.thumbnail,
+                orderId:paymentResponse.id,
+                currency:paymentResponse.currency,
+                amount:paymentResponse.amount
+            })
+        }catch(error){
+            console.log("error occure at  apture payment fun");
+
+            return res.json({
+                sucess:true,
+                message:`Could not initiate  order :${error}`
+            })
+        }
     
-    // order create
-    //return response
-    
+}
+
+//verify Signature of the razorPay and server          (simple:server Secrect and razorpay sent Secret matching)
+exports.verifySignature= async(req,res)=>{
+    const webhookSecrect="omNamahshivay"; //my signature
+    const signature =req.headers("x-razorpay-signature"); //razorpay's signature hash form got
+
+     const shasum=crypto.createHmac("sha256",webhookSecrect);
+
+     shasum.update(JSON.stringify(req.body));
+     const digest=shasum.digest("hex");
+
+     if(signature===digest){
+        console.log("payment is authorized");
+        const {courseId,userId}=req.body.payload.payment.entity.notes;
+        try{
+            //find course and enroll student
+            const enrolledCourse=await Course.findByIdAndUpdate({_id:courseId},{$push:{studentEnrolled:{userId}}},{new:true});
+            if(!enrolledCourse){
+                return res.status(500).json({
+                    sucess:false,
+                    message:'course did not found'
+                })
+            }
+            console.log(enrolledCourse);
+
+            //find the student and add inside courese list  which enrolledCourse
+            const enrolledStudent=await User.findByIdAndUpdate({_id:userId},{$push:{Courses:courseId}},{new:true});
+            
+            console.log(enrolledStudent);
+            
+            //send confirmation mail
+            const emailResponse=await mailSender(enrolledStudent.email,"Succesfully enrolled",`congratulation u have suceesfully enrolled ${enrolledCourse.courseName}`);
+            console.log(emailResponse);
+            
+            return res.status(200).json({
+                sucess:true,
+                message:"signature verified AND course added"
+            })
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({
+                sucess:false,
+                message: `error at verifySignature function : ${error.message}`
+            });
+        }
+     }
+     else{
+        return res.status(400).json({
+            sucess:false,
+            message:"signature are diffrent"
+        })
+     }
 }
